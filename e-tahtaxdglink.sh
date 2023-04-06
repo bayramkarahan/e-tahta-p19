e52e34936a729684a14f2ebc8fe858b2ea190ab4
bayramkarahan <bayramk@gmail.com>
2023-04-06 14:09:19 +0300

    ini

--- e-tahtaxdglink.sh
+++ e-tahtaxdglink.sh
@@ -8,22 +8,25 @@ INIFILE="$HOME/e-tahta.ini"
 if [ -f $FILE ]; then
 cp /usr/share/applications/e-tahta.desktop $FILE
 chmod 777 $FILE
-chown $HOME:$HOME $FILE
+chown $USER:$USER $FILE
      echo "dosya var"
 else
 cp /usr/share/applications/e-tahta.desktop $FILE
 chmod 777 $FILE
-chown $HOME:$HOME $FILE
+chown $USER:$USER $FILE
 echo "Dosya Kopyalandı.."
    
 fi
 
 if [ -f $INIFILE ]; then
-     echo "dosya var"
+    	echo "dosya var"
+     	cp /usr/share/e-tahta/e-tahta.ini $INIFILE
+	chmod 777 $INIFILE
+	chown $USER:$USER $INIFILE
 else
 cp /usr/share/e-tahta/e-tahta.ini $INIFILE
 chmod 777 $INIFILE
-chown $HOME:$HOME $INIFILE
+chown $USER:$USER $INIFILE
 echo "Dosya Kopyalandı.."
    
 fi
